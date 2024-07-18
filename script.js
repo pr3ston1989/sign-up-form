ed13eae9037938b02f4710271ddeb23b0d37cd7b
Damian Pietrzak <{ID}+{username}@users.noreply.github.com>
2024-07-18 19:34:40 +0200

    Add password match checking and improve styles.

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ function validatePassword() {
     if (password != passwordConfirm) {
         document.getElementById("wrong_password").style.color = "red";
         document.getElementById("wrong_password").innerHTML = "* Passwords do not match";
+        return false;
     } else {
         document.getElementById("wrong_password").style.color = "green";
         document.getElementById("wrong_password").innerHTML = "* Passwords match";
+        return true;
     }
 }
+
+let button = document.querySelector("button");
+
+button.addEventListener("click", function(event) {
+    if (!validatePassword()) {
+        event.preventDefault();
+    }
+})
